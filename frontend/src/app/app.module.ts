48724b1db2ea57e2f2e7f604f69c2d3ffd0410bc
m32795434 <manubravard@gmail.com>
2021-08-31 16:40:03 -0300

    HttpClient instantiated, and .get method created in employee.service.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './components/employee/employee.component';
@@ -10,7 +11,7 @@ import { EmployeeComponent } from './components/employee/employee.component';
     EmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -1,15 +1,20 @@
 //CONNECTION TO EMPLOYEE API ROUTES.
 
 import { Injectable } from '@angular/core';
+//to interactuate with http requests (get, post, put, delete)
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
   URL_API = 'http://localhost:3000/api/employees';
-  constructor() { }
+  //instantiate the httpClient
+  constructor(private http: HttpClient) { }
+  
+  
   getEmployees() {
-    return []
+   return this.http.get(this.URL_API);
+   
   }
 }
